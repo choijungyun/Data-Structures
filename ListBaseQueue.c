b966a6820d15a5f5b89e3df852562735b3b4c646
choijungyun <cjy8529@naver.com>
2020-02-03 03:09:49 +0900

    리스트 기반의 큐

---
+++ ListBaseQueue.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListBaseQueue.h"
+
+void QueueInit(Queue * pq)
+{
+	pq->front = NULL;
+	pq->rear = NULL;
+}
+
+int QIsEmpty(Queue * pq)
+{
+	if(pq->front == NULL)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void Enqueue(Queue * pq, Data data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	newNode->next = NULL;
+	newNode->data = data;
+
+	if(QIsEmpty(pq))
+	{
+		pq->front = newNode;
+		pq->rear = newNode;
+	}
+	else
+	{
+		pq->rear->next = newNode;
+		pq->rear = newNode;
+	}
+}
+
+Data Dequeue(Queue * pq)
+{
+	Node * delNode;
+	Data retData;
+
+	if(QIsEmpty(pq))
+	{
+		printf("Queue Memory Error!");
+		exit(-1);
+	}
+
+	delNode = pq->front;
+	retData = delNode->data;
+	pq->front = pq->front->next;
+	//front만 처리하고 rear는 처리 안함
+	//f만 참조해서 함수를 구성하기 때문에 굳이 r을 신경 쓸 필요가 없다
+
+	free(delNode);
+	return retData;
+}
+
+Data QPeek(Queue * pq)
+{
+	if(QIsEmpty(pq))
+	{
+		printf("Queue Memory Error!");
+		exit(-1);
+	}
+
+	return pq->front->data;
+}
